322bd614d536b328caad6500d64ae0bcc4671904
norio <nakasai@all-rounder.biz>
2016-01-19 21:15:28 +0900

    interceptor

--- src/main/java/biz/allrounder/jee7sample/resources/LoggingInterceptor.java
+++ src/main/java/biz/allrounder/jee7sample/resources/LoggingInterceptor.java
@@ -1,6 +1,11 @@
 package biz.allrounder.jee7sample.resources;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.annotation.Priority;
 import javax.enterprise.context.Dependent;
@@ -9,6 +14,11 @@ import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -22,18 +32,35 @@ public class LoggingInterceptor {
 	private UserId userId;
 	@Inject
 	private HttpServletRequest req;	
+	
+	private static final List<Class<?>> restEasyParamClasses = Arrays.asList(
+			PathParam.class,
+			QueryParam.class,
+			HeaderParam.class,
+			MatrixParam.class,
+			CookieParam.class);
 
 	@AroundInvoke
     public Object invoke(final InvocationContext ctx) throws Throwable {
-		System.out.println("aiueo");
 		
 		if (ctx.getMethod().isAnnotationPresent(Logging.class)) {
 			Logging logging = ctx.getMethod().getAnnotation(Logging.class);
 			System.out.println(logging.operationName());
 		}
 		
+		List<Type> jsonTypeList = new ArrayList<>();
+		for (Parameter param: ctx.getMethod().getParameters()) {
+			Annotation[] anons = param.getAnnotations();
+			for (Annotation anon: anons) {
+				if (restEasyParamClasses.contains(anon)) {
+					continue;
+				}
+				jsonTypeList.add(param.getType());
+			}
+		}
+
 		for (Object object: ctx.getParameters()) {
-			if (object.getClass().getAnnotation(JsonObject.class) != null) {
+			if (jsonTypeList.contains(object.getClass())) {
 				String json = new ObjectMapper().writeValueAsString(object);
 				System.out.println(json);
 			}
